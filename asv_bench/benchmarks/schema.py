f8690b68ac326b320109a3643e51aba623a62b22
Marco Neumann <marco.neumann@blue-yonder.com>
2019-05-08 17:44:09 +0200

    Fix ASV benchmarks
    Sadly, we cannot use ASV in CI because it would not report such
    failures. See https://github.com/airspeed-velocity/asv/issues/449

--- asv_bench/benchmarks/schema.py
+++ asv_bench/benchmarks/schema.py
@@ -18,7 +18,7 @@ class TimeMakeMeta(AsvBenchmarkConfig):
         self.df = get_dataframe_alltypes()
 
     def time_make_meta(self):
-        make_meta(self.df)
+        make_meta(self.df, origin="df")
 
 
 class TimeValidateCompatible(AsvBenchmarkConfig):
@@ -30,10 +30,10 @@ class TimeValidateCompatible(AsvBenchmarkConfig):
 
     def setup(self, num_schemas, has_na):
         self.df = get_dataframe_alltypes()
-        schema = make_meta(self.df)
+        schema = make_meta(self.df, origin="df")
         self.schemas = [deepcopy(schema) for _ in range(num_schemas)]
         if has_na:
-            empty_schema = make_meta(self.df[0:0])
+            empty_schema = make_meta(self.df[0:0], origin="empty")
             # insert alternating empty schemas
             self.schemas[::2] = [
                 deepcopy(empty_schema) for _ in range(len(self.schemas[::2]))
@@ -57,7 +57,7 @@ class TimeValidateSharedColumns(AsvBenchmarkConfig):
 
     def setup(self, num_schemas):
         self.df = get_dataframe_alltypes()
-        schema = make_meta(self.df)
+        schema = make_meta(self.df, origin="df")
         self.schemas = [deepcopy(schema) for _ in range(num_schemas)]
 
     def peakmem_validate_shared_columns(self, num_schemas):
