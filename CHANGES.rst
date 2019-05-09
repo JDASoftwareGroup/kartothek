678a966255f295f2a3e205ab9a0e66f6ebdf65ff
Marco Neumann <marco.neumann@blue-yonder.com>
2019-05-09 14:08:12 +0200

    fix getargspec deprecation warning

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,6 +2,12 @@
 Changelog
 =========
 
+Version 3.0.1 (2019-XX-XX)
+==========================
+
+- fix ``getargspec`` ``DeprecationWarning``
+
+
 Version 3.0.0 (2019-05-02)
 ==========================
 
--- kartothek/core/_mixins.py
+++ kartothek/core/_mixins.py
@@ -1,18 +1,16 @@
 # -*- coding: utf-8 -*-
-
-
+import collections
 import inspect
-import logging
-
-_logger = logging.getLogger(__name__)
 
 
 class CopyMixin(object):
     def copy(self, **kwargs):
-        constructor_args = inspect.getargspec(self.__init__).args
-        init_args = {}
-        # first arg is always `self`
-        for arg in constructor_args[1:]:
+        constructor_args = inspect.signature(self.__init__).parameters
+        init_args = collections.OrderedDict()
+
+        for arg in constructor_args.keys():
+            if arg == "self":
+                continue
             init_args[arg] = kwargs.get(arg, getattr(self, arg, None))
 
         return type(self)(**init_args)
