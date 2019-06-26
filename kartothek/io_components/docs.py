9a41f9fab39c3bd81fefe5a613c7ea12ee1b38ea
fjetter <fjetter@users.noreply.github.com>
2019-06-26 10:47:41 +0200

    Update kartothek/io_components/docs.py
    Co-Authored-By: Marco Neumann <marco@crepererum.net>

--- kartothek/io_components/docs.py
+++ kartothek/io_components/docs.py
@@ -64,7 +64,7 @@ _PARAMETER_MAPPING = {
 """,
     "partition_on": """
     partition_on: list
-        Column names by which the dataset should be partitioned by.
+         Column names by which the dataset should be partitioned by physically.
         These columns may later on be used as an Index to improve query performance.
         Partition columns need to be present in all dataset tables.
         Sensitive to ordering.
