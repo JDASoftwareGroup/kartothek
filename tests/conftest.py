c3d21c58b2502b956e3d4b0477b169f56ba7457b
fjetter <fjetter@users.noreply.github.com>
2019-07-04 13:52:28 +0200

    Enable faulthandler

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,10 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=C0103, C0111, W0621
 
+# fmt: off
+import faulthandler  # noqa: E402 isort:skip
+faulthandler.enable()  # noqa: E402 isort:skip
+# fmt: on
 
 import datetime
 from collections import OrderedDict
