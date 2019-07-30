d6e80fbb7349418a40dd91d0be87d2dfdb8ac235
Ganesh M <47482214+mganesh1308@users.noreply.github.com>
2019-07-30 13:15:10 +0200

    pyarrow nightly buidv16

--- ci/set_numpy_pandas_integration.sh
+++ ci/set_numpy_pandas_integration.sh
@@ -2,9 +2,9 @@
 
 set -xeo pipefail
 
-pip-compile ./ci/numpy_pandas_requirements.txt
+pip-compile ./ci/numpy_pandas_requirements.txt -o ./ci/numpy_pandas_req_pinned.txt
 
-pip install -r ./ci/numpy_pandas_requirements.txt
+pip install -r ./ci/numpy_pandas_req_pinned.txt
 
 echo "Upgrading to  Nightly build of numpy pandas"
 
@@ -13,3 +13,5 @@ PRE_WHEELS="https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf
 pip install --pre --no-deps --upgrade --timeout=180 --no-cache-dir -f $PRE_WHEELS numpy pandas
 
 pip freeze
+
+rm -f ./ci/numpy_pandas_req_pinned.txt
--- ci/set_pyarrow_integration.sh
+++ ci/set_pyarrow_integration.sh
@@ -2,12 +2,16 @@
 
 set -xeo pipefail
 
-pip-compile ./ci/pyarrow_requirements.txt
+pip-compile ./ci/pyarrow_requirements.txt -o ./ci/pyarrow_req_pinned.txt
 
-pip install -r ./ci/integration_requirements.txt
+pip install -r ./ci/pyarrow_req_pinned.txt
 
 echo "Upgrading to Nightly build of pyarrow"
 
 pip install --pre --no-deps --upgrade --timeout=180 --no-cache-dir -f "https://github.com/ursa-labs/crossbow/releases/download/latest" pyarrow
 
 pip freeze
+
+rm -f ./ci/pyarrow_req_pinned.txt
+
+
