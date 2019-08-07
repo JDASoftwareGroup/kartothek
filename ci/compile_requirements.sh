5b84cac606889a71dcf641eb16cf096e8778c828
Marco Neumann <marco.neumann@blue-yonder.com>
2019-08-07 18:06:23 +0200

    add nightly integration with numpy and pandas

--- ci/compile_requirements.sh
+++ ci/compile_requirements.sh
@@ -2,11 +2,13 @@
 set -xeo pipefail
 
 pip-compile \
+    -v \
     $PIP_COMPILE_ARGS \
     -o requirements-pinned.txt \
     requirements.txt
 
 pip-compile \
+    -v \
     $PIP_COMPILE_ARGS \
     -o test-requirements-pinned.txt \
     requirements-pinned.txt \
