65cf6a35ad050205dc7d888caae834ed180646c9
Marco Neumann <marco.neumann@blue-yonder.com>
2019-08-16 10:55:47 +0200

    clarify some CI things

--- ci/compile_requirements.sh
+++ ci/compile_requirements.sh
@@ -5,7 +5,7 @@ if [ -z ${PIP_COMPILE_ARGS+x} ]; then
     PIP_COMPILE_ARGS=""
 fi
 
-if [[ "${ARROW_NIGHTLY+1}" == 1 ]]; then
+if [[ "${ARROW_NIGHTLY-0}" == 1 ]]; then
     ARROW_URL="$(./ci/get_pyarrow_nightly.py)"
     ARROW_FILE="$(basename $ARROW_URL)"
     ARROW_VERSION="$(echo "$ARROW_FILE" | awk '{split($1,a,"-"); print a[2]}')"
@@ -16,6 +16,11 @@ if [[ "${ARROW_NIGHTLY+1}" == 1 ]]; then
     wget -O "$ARROW_PATH" "$ARROW_URL"
 
     PIP_COMPILE_ARGS="$PIP_COMPILE_ARGS -P pyarrow==$ARROW_VERSION -f file://$PACKAGE_PATH"
+fi
+
+if [[ "${NUMFOCUS_NIGHTLY-0}" == 1 ]]; then
+    # NumFOCUS nightly wheels, contains numpy and pandas
+    PIP_COMPILE_ARGS="$PIP_COMPILE_ARGS --pre -f https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com"
 fi
 
 pip-compile \
