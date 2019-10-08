00ad897b91ec737cd2d94abb7efd44a51d9a4ce9
Marco Neumann <marco.neumann@blue-yonder.com>
2019-10-08 17:55:11 +0200

    fix pyarrow 0.15.0 deprecation

--- kartothek/core/_compat.py
+++ kartothek/core/_compat.py
@@ -7,6 +7,7 @@ import simplejson
 
 ARROW_LARGER_EQ_0130 = LooseVersion(pa.__version__) >= "0.13.0"
 ARROW_LARGER_EQ_0141 = LooseVersion(pa.__version__) >= "0.14.1"
+ARROW_LARGER_EQ_0150 = LooseVersion(pa.__version__) >= "0.15.0"
 DASK_LARGER_EQ_121 = LooseVersion(dask.__version__) >= "1.2.1"
 
 
