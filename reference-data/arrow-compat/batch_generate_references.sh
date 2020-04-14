720ac9738a0fabd3a00f8fe9f7662a664b98f17a
lr4d <lr4d@users.noreply.github.com>
2020-04-14 15:34:21 +0200

    update reference Parquet generation scripts

---
+++ reference-data/arrow-compat/batch_generate_references.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Note: this assumes you have kartothek installed in your current environment and you are using conda
+
+PYARROW_VERSIONS="0.14.1 0.15.0 0.16.0"
+
+for pyarrow_version in $PYARROW_VERSIONS; do
+    echo $pyarrow_version 
+    conda install -y pyarrow==$pyarrow_version
+    ./generate_reference.py || (echo "Failed for version $pyarrow_version"; exit 1)
+done
