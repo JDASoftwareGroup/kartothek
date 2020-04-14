b7ea769ba87114f801a422ec8638df3824ebdf9a
lr4d <lr4d@users.noreply.github.com>
2020-04-14 15:34:41 +0200

    update reference Parquet files

--- reference-data/arrow-compat/batch_generate_references.sh
+++ reference-data/arrow-compat/batch_generate_references.sh
@@ -5,7 +5,7 @@
 PYARROW_VERSIONS="0.14.1 0.15.0 0.16.0"
 
 for pyarrow_version in $PYARROW_VERSIONS; do
-    echo $pyarrow_version 
+    echo $pyarrow_version
     conda install -y pyarrow==$pyarrow_version
     ./generate_reference.py || (echo "Failed for version $pyarrow_version"; exit 1)
 done
