29cf5769fe4b179207e17c436481403129bb91b4
Florian Jetter <fjetter@users.noreply.github.com>
2020-06-10 07:07:01 +0200

    Add package metadata flag for python 3.8

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ def setup_package():
             "Programming Language :: Python :: 3",
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
         ],
         use_scm_version=True,
         long_description=open("README.md", "r").read(),
