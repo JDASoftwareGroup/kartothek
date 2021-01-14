ed223260857c7a08a1b2011917143bd60e29d0e3
Marco Neumann <57095103+marco-neumann-by@users.noreply.github.com>
2021-01-14 09:49:58 +0100

    fix master (#391)
    * fix fixture scopes for hypothesis
    
    * fix nightly numpy typing issues
    
    * fix pandas-pyarrow interaction
    
    * run numfocus nightly tests with pyarrow 2.0.0 instead of 0.17.1

--- tests/io/cube/conftest.py
+++ tests/io/cube/conftest.py
@@ -2,7 +2,9 @@ import dask
 import pytest
 
 
-@pytest.fixture(params=["dask_bag_bs1", "dask_bag_bs3", "dask_dataframe", "eager"])
+@pytest.fixture(
+    params=["dask_bag_bs1", "dask_bag_bs3", "dask_dataframe", "eager"], scope="session"
+)
 def driver_name(request):
     return request.param
 
@@ -12,7 +14,7 @@ def setup_dask():
     dask.config.set(scheduler="synchronous")
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def skip_eager(driver_name):
     if driver_name == "eager":
         pytest.skip("Skipped for eager backend.")
