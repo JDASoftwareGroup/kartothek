e7d3f84886cf7c00879d80ef1e742bd76f42d327
Stephan Heßelmann (lgtf/39809) <stephan.hesselmann@blueyonder.com>
2021-02-22 22:26:09 +0100

    Expand unit tests

--- tests/io/dask/test_common_cube.py
+++ tests/io/dask/test_common_cube.py
@@ -1,17 +1,22 @@
+import pytest
+
 from kartothek.core.cube.cube import Cube
 from kartothek.core.dataset import DatasetMetadata
 from kartothek.io.dask.common_cube import ensure_valid_cube_indices
 
 
 class FakeSeedTableMetadata:
-    names = ["d1", "d2", "p", "i"]
+    names = ["d1", "d2", "p", "i1", "i2"]
 
 
 class FakeExtraTableMetadata:
-    names = ["d1", "p", "i"]
+    names = ["d1", "p", "i1"]
 
 
-def test_cube_indices_are_validated():
+def test_cube_with_valid_indices_is_not_modified_by_validation():
+    """
+    Test that a cube with valid indices is not modified by `ensure_valid_cube_indices`
+    """
     source_metadata = DatasetMetadata.from_dict(
         {
             "dataset_uuid": "source",
@@ -21,7 +26,7 @@ def test_cube_indices_are_validated():
             "indices": {
                 "d1": {"1": ["part_1"]},
                 "d2": {"1": ["part_1"]},
-                "i": {"1": ["part_1"]},
+                "i1": {"1": ["part_1"]},
             },
         }
     )
@@ -31,7 +36,7 @@ def test_cube_indices_are_validated():
             "dataset_metadata_version": 4,
             "table_meta": {"table": FakeExtraTableMetadata()},
             "partition_keys": ["p"],
-            "indices": {"i": {"1": ["part_1"]}},
+            "indices": {"i1": {"1": ["part_1"]}},
         }
     )
     cube = Cube(
@@ -39,7 +44,7 @@ def test_cube_indices_are_validated():
         partition_columns=["p"],
         uuid_prefix="cube",
         seed_dataset="source",
-        index_columns=["i"],
+        index_columns=["i1"],
     )
 
     validated_cube = ensure_valid_cube_indices(
@@ -47,3 +52,85 @@ def test_cube_indices_are_validated():
     )
 
     assert validated_cube == cube
+
+
+def test_existing_indices_are_added_when_missing_in_cube():
+    """
+    Test that indices already existing in the dataset are added to the validated cube
+    """
+    source_metadata = DatasetMetadata.from_dict(
+        {
+            "dataset_uuid": "source",
+            "dataset_metadata_version": 4,
+            "table_meta": {"table": FakeSeedTableMetadata()},
+            "partition_keys": ["p"],
+            "indices": {
+                "d1": {"1": ["part_1"]},
+                "d2": {"1": ["part_1"]},
+                "i1": {"1": ["part_1"]},
+                "i2": {"1": ["part_1"]},
+            },
+        }
+    )
+    extra_metadata = DatasetMetadata.from_dict(
+        {
+            "dataset_uuid": "extra",
+            "dataset_metadata_version": 4,
+            "table_meta": {"table": FakeExtraTableMetadata()},
+            "partition_keys": ["p"],
+            "indices": {"i1": {"1": ["part_1"]}},
+        }
+    )
+    cube = Cube(
+        dimension_columns=["d1", "d2"],
+        partition_columns=["p"],
+        uuid_prefix="cube",
+        seed_dataset="source",
+        index_columns=["i1"],
+    )
+
+    validated_cube = ensure_valid_cube_indices(
+        {"source": source_metadata, "extra": extra_metadata}, cube
+    )
+
+    assert validated_cube.index_columns == {"i1", "i2"}
+
+
+def test_raises_when_cube_defines_index_not_in_dataset():
+    """
+    Test that a `ValueError` is raised when the cube defines an index that is not part of a dataset
+    """
+    source_metadata = DatasetMetadata.from_dict(
+        {
+            "dataset_uuid": "source",
+            "dataset_metadata_version": 4,
+            "table_meta": {"table": FakeSeedTableMetadata()},
+            "partition_keys": ["p"],
+            "indices": {
+                "d1": {"1": ["part_1"]},
+                "d2": {"1": ["part_1"]},
+                "i1": {"1": ["part_1"]},
+            },
+        }
+    )
+    extra_metadata = DatasetMetadata.from_dict(
+        {
+            "dataset_uuid": "extra",
+            "dataset_metadata_version": 4,
+            "table_meta": {"table": FakeExtraTableMetadata()},
+            "partition_keys": ["p"],
+            "indices": {"i1": {"1": ["part_1"]}},
+        }
+    )
+    cube = Cube(
+        dimension_columns=["d1", "d2"],
+        partition_columns=["p"],
+        uuid_prefix="cube",
+        seed_dataset="source",
+        index_columns=["i2"],
+    )
+
+    with pytest.raises(ValueError):
+        ensure_valid_cube_indices(
+            {"source": source_metadata, "extra": extra_metadata}, cube
+        )
